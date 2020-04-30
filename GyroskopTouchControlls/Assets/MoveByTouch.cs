795dfa6d7d8488b9d26a10ddaa108dcac036460d
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-04-30 15:05:38 +0200

    Tried out Gyroscope, Touch and Accelorometer Controlls

---
+++ GyroskopTouchControlls/Assets/MoveByTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByTouch : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); //get the first touch 
+            //touch methods
+            //touch.position;
+            // touch.phase --> began, ended, moved, stationary, cancelled
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0f;
+            transform.position = touchPosition;
+        }
+    }
+}
