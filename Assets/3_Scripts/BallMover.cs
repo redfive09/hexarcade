e9991e6307282245a58a816a17cd6fb4c2a17f7b
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-05-05 21:42:02 +0200

    Ball can be moved with wasd or arrow keys
    - added BallMover.cs
    - adjusted speed to 500

---
+++ Assets/3_Scripts/BallMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+public class BallMover : MonoBehaviour 
+{
+
+    public float speed = 800.0f;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce (movement * (speed * Time.fixedDeltaTime));
+    }
+
+}
