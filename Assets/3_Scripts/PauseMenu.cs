a2ea625863668d7ff1d9e4a61da4ddc397d1b483
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-05-10 15:13:13 +0200

    TextMP, Pause Menu, Animation Folder, Build Settings
    - Pause Mode
    --> PauseMenu.cs attached to Canvas
    - new bad Manu Scene for testing in Scenes folder
    - downloaded TextMeshPro for UI improvement
    - Pause Animation.anim Script in 5_Animation Folder for fade effect
    - access Pause bei pressing Space **Fix** --> Pause button in scene (because phone)

---
+++ Assets/3_Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    /*
+     * Tutorial used: https://youtu.be/JivuXdrIHK0
+     */
+    public static bool GameIsCurrentlyPaused = false;
+
+    [SerializeField] GameObject pauseMenuUI;
+
+    private String sceneToLoad = "MenuScene"; //I just put in a fake Menu Scene to test
+    // Scene to load can be changed later 
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsCurrentlyPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() //public to be able to call it from the button
+    {
+        pauseMenuUI.SetActive(false); //disable Pause Menu (Child of the Canvas this script is linked to 
+        Time.timeScale = 1f; // normal time
+        GameIsCurrentlyPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true); //enable Pause Menu (Child of the Canvas this script is linked to 
+        Time.timeScale = 0f; // Stop time
+        GameIsCurrentlyPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f; // normal time
+        SceneManager.LoadScene(sceneToLoad); //load any scene (in this case menu scene)
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quit"); //Application.Quit does nothing visible, so I left the Debug.Log statement
+        Application.Quit();
+    }
+}
