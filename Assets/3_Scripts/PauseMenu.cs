482a649c5a8b9552a2d1bdbb49b6b990832d3661
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-05-10 17:25:58 +0200

    now it's working apparently :/

--- Assets/3_Scripts/PauseMenu.cs
+++ Assets/3_Scripts/PauseMenu.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -9,12 +11,30 @@ public class PauseMenu : MonoBehaviour
      * Tutorial used: https://youtu.be/JivuXdrIHK0
      */
     public static bool GameIsCurrentlyPaused = false;
-
+    public Button pauseButton;
     [SerializeField] GameObject pauseMenuUI;
 
     private String sceneToLoad = "MenuScene"; //I just put in a fake Menu Scene to test
     // Scene to load can be changed later 
-    
+
+    void Start () {
+        Button btn = pauseButton.GetComponent<Button>();
+        btn.onClick.AddListener(Pause);
+    }
+    void Update()
+    {
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (GameIsCurrentlyPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }*/
+    }
     public void Resume() //public to be able to call it from the button
     {
         pauseMenuUI.SetActive(false); //disable Pause Menu (Child of the Canvas this script is linked to 
