59eb10822be637e1641528acc2b6ce61818e09dc
maidjerry <michaelschlesener@googlemail.com>
2020-05-12 17:18:34 +0200

    add test scene with alternative ball control

---
+++ Assets/3_Scripts/ObjectManipulation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class ObjectManipulation : MonoBehaviour
+{
+
+    public GameObject playingPiece;
+
+    [SerializeField] private float rotationPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotation = Time.deltaTime * rotationPower;
+        playingPiece.transform.Rotate(90.0f * rotation, 0.0f * rotation, 0.0f * rotation, Space.Self);
+    }
+}
