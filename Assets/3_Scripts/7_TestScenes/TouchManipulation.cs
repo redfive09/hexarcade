4ab8fe9d9ec5e362c5ebe7f58d7f2207f33f08b4
maidjerry <michaelschlesener@googlemail.com>
2020-05-27 20:07:45 +0200

    clean up touch input scene

---
+++ Assets/3_Scripts/7_TestScenes/TouchManipulation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManipulation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                hitInfo.collider.GetComponent<Collider>().gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/3_Scripts/7_TestScenes/WriteTouchCoords.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class WriteTouchCoords : MonoBehaviour
+{
+    
+    [SerializeField] private Text touchInfo = null;
+    [SerializeField] private Text touchInfoRaw = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchCoords = touch.position; // .position changes if the touch moves on scree
+            Vector3 touchCoordsRaw = touch.rawPosition; // .rawposition does not change but remains the initial touch position
+            touchInfo.text = "___ x: " + touchCoords.x + " y: " + touchCoords.y;
+            touchInfoRaw.text = "Raw: x: " + touchCoordsRaw.x + " y: " + touchCoordsRaw.y;
+        }
+    }
+}
+
