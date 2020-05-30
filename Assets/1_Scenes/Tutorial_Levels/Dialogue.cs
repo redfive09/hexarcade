959410f8fab54d903319d25def33ab3981f6144e
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-05-30 20:11:26 +0200

    Tutorial, LeanTween Library, PauseMenu
    - langer Versuch ein ordentliches Ttorial hinzubekommen
    - neuer Build Ordner im Projekt
    - Pause Menü gefixed (lang nur ein bisschen an der hierarchie
    - LeanTween Library: Soll zum Animieren von UI effizienter sein, als Animationen, weil Tweens nicht jede Sekunde gerufen werden

---
+++ Assets/1_Scenes/Tutorial_Levels/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿
+    using UnityEngine;
+
+    [System.Serializable]
+    public class Dialogue
+    {
+        [TextArea(3,10)]
+        public string[] sentences;
+     //   public string name;
+
+    }
