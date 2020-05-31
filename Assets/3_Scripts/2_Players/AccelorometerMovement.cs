79575afa4b9af90b5ca24ab158069d159e1080a6
maidjerry <michaelschlesener@googlemail.com>
2020-05-31 21:12:16 +0200

    polish control

--- Assets/3_Scripts/2_Players/AccelorometerMovement.cs
+++ Assets/3_Scripts/2_Players/AccelorometerMovement.cs
@@ -11,19 +11,30 @@ public class AccelorometerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     public bool isFlat = true;
-    void Start()
+    [SerializeField]
+    private float multiplier = 1.0f;
+    [SerializeField]
+    private ForceMode inputApplyment = ForceMode.Acceleration;
+
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        
     }
 
     void Update()
     {
         Vector3 tilt = Input.acceleration;
+        tilt.z = 0.0f;
         if (isFlat)
         {
             tilt = Quaternion.Euler(90, 0, 0) * tilt;
         }
-        rb.AddForce(tilt);
-        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+        tilt *= multiplier;
+        rb.AddForce(tilt, inputApplyment);
     }
 }
