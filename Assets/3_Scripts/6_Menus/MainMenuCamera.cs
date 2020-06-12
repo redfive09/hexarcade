7108446c0b0b94669479f46533ca2bd76c7bb8cd
maidjerry <michaelschlesener@googlemail.com>
2020-06-12 13:42:22 +0200

    add animation of ground in main menu

---
+++ Assets/3_Scripts/6_Menus/MainMenuCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenuCamera : MonoBehaviour
+{
+    [SerializeField]
+    AnimationCurve movement; //ymax = 0.475911
+    private Transform ThisTransform;
+    private Vector3 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThisTransform = GetComponent<Transform>();
+        initialPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThisTransform.position = initialPosition + Vector3.forward * movement.Evaluate(Time.time);
+    }
+}
