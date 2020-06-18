0956d634c698f49983c385772352b4e9c9946450
loxer <loxer@live.de>
2020-06-19 01:03:20 +0200

    Finished algorithm of finding worlds and its levels

--- Assets/3_Scripts/6_Menus/LevelSelection.cs
+++ Assets/3_Scripts/6_Menus/LevelSelection.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
@@ -7,16 +6,17 @@ using TMPro;
 
 public class LevelSelection : MonoBehaviour
 {
-    [SerializeField] private GameObject timeRecords;    
-    [SerializeField] private GameObject menuPage01;
-    [SerializeField] private GameObject menuPage02;
-    [SerializeField] private GameObject LevelAtPos1;
+    // [SerializeField] private GameObject timeRecords;    
+    // [SerializeField] private GameObject menuPage01;
+    // [SerializeField] private GameObject menuPage02;
+    // [SerializeField] private GameObject LevelAtPos1;
 
 
     private Dictionary<string, float> bestTimes;
-    private bool switchToFirstPage = true;
+    private Dictionary<string, List<string>> worlds = new Dictionary<string, List<string>>();
+    private SortedSet<string> worldList = new SortedSet<string>();
     
-
+    
     // Settings for control
     private const float MIN_DISTANCE_FOR_PANNING_RECOGNITION = 0.05f;
     private Camera cam;
@@ -32,6 +32,7 @@ public class LevelSelection : MonoBehaviour
         bestTimes = SaveLoadManager.LoadTimes();
         // ShowTimeRecords();
         // SwitchPage();
+        ManageLevels();
     }
 
     private void Update()
@@ -72,10 +73,67 @@ public class LevelSelection : MonoBehaviour
         }
     }
 
+
+    private void ManageLevels()
+    {        
+        int skipNamePart = ".unity".Length;                                             // every scene ends with that extension, so we can remember it (respectively its length) in order to save some time
+                
+        for(int i = 3; i < SceneManager.sceneCountInBuildSettings - 1; i++)             // first level starts at build 3 and last scene is not a level either
+        {
+            string scenePath = SceneUtility.GetScenePathByBuildIndex(i);                // thx to this -> https://stackoverflow.com/questions/40898310/get-name-of-next-scene-in-build-settings-in-unity3d/40901893
+            string worldName = "";
+            string levelName = "";
+            int firstSlashAtIndex = 0;
+            int indexEndOfLevelName = scenePath.Length - skipNamePart - 1;              // we need -1, because the index starts at 0
+            
+            for(int j = indexEndOfLevelName; j >= 0; j--)
+            {                
+                string namePart = scenePath.Substring(j, 1);                            // get a single char, which we need for checking, if it's a slash character
+                if(namePart == "/" && firstSlashAtIndex == 0)                           // if it hits the first slash, then we found the beginning of the level name
+                {
+                    firstSlashAtIndex = j;                                              // remember the position, we need it to differentiate between the world and level names
+                    levelName = scenePath.Substring(j + 1, indexEndOfLevelName - j);    // we don't need the slash itself, that's why we need +1; the substraction returns us the amount of steps we gone so far
+                }
+                else if (namePart == "/" && firstSlashAtIndex != 0)                     // if it wasn't the first slash, then we found the beginning of the world name
+                {
+                    worldName = scenePath.Substring(j + 1, firstSlashAtIndex - j - 1);  // -1 since we don't want the slash-char to be part of the name
+                    
+                    if(worlds.TryGetValue(worldName, out List<string> levels))          // check, if we saved the world already
+                    {
+                        levels.Add(levelName);                                          // add the level to the existing world
+                    }
+                    else                                                                // in case the world does not exist, yet:
+                    {
+                        List<string> levelList = new List<string>();                    // create a new list for its levels
+                        levelList.Add(levelName);                                       // add the new level
+                        worlds[worldName] = levelList;                                  // give the level list to the world
+                        worldList.Add(worldName);                                      // and save the world name as well
+                    }
+                    break;                                                              // we found everything we need, so let's go to the next level
+                }
+            }
+        }
+    }
+
+    private void PrintAllLevels()
+    {
+        foreach (string world in worldList)
+        {
+            List<string> levelList = worlds[world];
+
+            foreach (string level in levelList)
+            {
+                Debug.Log(level + " in " + world);
+            }
+        }
+    }
+
+
+
     // Shows time records below their corresponding level
     private void ShowTimeRecords()
     {
-        TextMeshProUGUI[] records = timeRecords.GetComponentsInChildren<TextMeshProUGUI>();
+        // TextMeshProUGUI[] records = timeRecords.GetComponentsInChildren<TextMeshProUGUI>();
 
         // for (int i = 0; i < records.Length; i++)
         // {
@@ -91,21 +149,8 @@ public class LevelSelection : MonoBehaviour
         // }
     }
 
-    public void SwitchPage()
-    {
-        if(switchToFirstPage)
-        {
-            menuPage02.SetActive(false);
-            menuPage01.SetActive(true);
-        }
-        else
-        {
-            menuPage01.SetActive(false);
-            menuPage02.SetActive(true);
-        }
-        ShowTimeRecords();
-        switchToFirstPage = !switchToFirstPage;
-    }
+
+
 
     public void ResetRecords()
     {
@@ -115,19 +160,19 @@ public class LevelSelection : MonoBehaviour
     }
 
     //Show Level 1 from Scenes In Build
-    public void Level01()
+    public void Level1()
     {
-        LevelAtPos1.transform.position = new Vector3(LevelAtPos1.transform.position.x - 100, 0, 0);
+        // LevelAtPos1.transform.position = new Vector3(LevelAtPos1.transform.position.x - 100, 0, 0);
     }
 
     //Show Level 2 from Scenes In Build
-    public void Level02()
+    public void Level2()
     {
         LoadLevel(2);
     }
 
     //Show Level 3 from Scenes In Build
-    public void Level03()
+    public void Level3()
     {
         LoadLevel(3);
     }
